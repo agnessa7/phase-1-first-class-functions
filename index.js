3940753cba1fcd7c394cf05d6c43053942894986
Agnessa Nikolayev <anikolayev90@gmail.com>
2023-04-07 07:22:41 -0700

    done

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+const receivesAFunction = function(holler) {
+
+    console.log(holler("Hey there!"))
+
+}
+
+
+
+function returnsANamedFunction(){
+    return function named(){}
+}
+
+
+
+ 
+
+  function returnsAnAnonymousFunction(){
+    return function(){}
+  }
+
+
+  
